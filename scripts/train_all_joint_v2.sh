cf0626d254feb9051e8c8485bd2e83d1dc57e468
ieellee <56500039+ieellee@users.noreply.github.com>
2025-04-21 14:30:06 +0800

    Update train_all_joint_v2.sh

--- scripts/train_all_joint_v2.sh
+++ scripts/train_all_joint_v2.sh
@@ -4,7 +4,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node=4 --master_port 29501 tra
     --log_interval 1000 \
     --sam_max_point_bs 4 \
     --learning_rate 0.001 \
-    --data_path "/home/user9/project/checkpoints/custom/uLLSAM/data/train_seg_all.jsonl" \
-    --sam_checkpoint "/home/user9/project/checkpoints/custom/uLLSAM/checkpoints/vit_b.pt" \
-    --segment_llm_path "/home/user9/project/checkpoints/custom/uLLSAM/checkpoints/sft_all_e1.pt" \
+    --data_path "./data/train_seg_all.jsonl" \
+    --sam_checkpoint "./checkpoints/vit_b.pt" \
+    --segment_llm_path "./checkpoints/sft_all_e1.pt" \
     --freeze_vision_projection False
